2dc0f4d687fa2e8992cfc921e2735c63c88e7d9f
Shafiek31 <43640566+Shafiek31@users.noreply.github.com>
2020-05-25 10:52:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// APA: Generate random background color by clicking on link. (2017, 12 oktober). Geraadpleegd op 18 mei 2020, van https://stackoverflow.com/questions/46716945/generate-random-background-color-by-clicking-on-link
+// Kleur bij klik: Generate random background color by clicking on link. (2017, 12 oktober). Geraadpleegd op 18 mei 2020, van https://stackoverflow.com/questions/46716945/generate-random-background-color-by-clicking-on-link
 
 //elke letter is apart om erop te klikken
 var letterL = document.querySelector('.l-letter');
@@ -10,57 +10,58 @@ var section02 = document.querySelector('.section-02');
 var section01 = document.querySelector('.section-01');
 var letterY = document.querySelector('y-letter');
 
-// continu klikken betekent kleur verandering
+// continu klikken = kleur verandering
 
 // letter L
 letterL.onclick = function () {
- var red = Math.floor(Math.random() * 256);
- var blue = Math.floor(Math.random() * 256);
- var green = Math.floor(Math.random() * 256);
+    var red = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
 
- this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+    this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 // letter I
 letterI.onclick = function () {
- var red = Math.floor(Math.random() * 256);
- var blue = Math.floor(Math.random() * 256);
- var green = Math.floor(Math.random() * 256);
+    var red = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
 
- this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+    this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 // letter G
 letterG.onclick = function () {
- var red = Math.floor(Math.random() * 256);
- var blue = Math.floor(Math.random() * 256);
- var green = Math.floor(Math.random() * 256);
+    var red = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
 
- this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+    this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 // letter G
 letterH.onclick = function () {
- var red = Math.floor(Math.random() * 256);
- var blue = Math.floor(Math.random() * 256);
- var green = Math.floor(Math.random() * 256);
+    var red = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
 
- this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+    this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 // letter T
 letterT.onclick = function () {
- var red = Math.floor(Math.random() * 256);
- var blue = Math.floor(Math.random() * 256);
- var green = Math.floor(Math.random() * 256);
+    var red = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
 
- this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+    this.style.color = "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 
+//Keyboards
 //bron: Hulp van Danny de Vries
 function toggle(event) {
-    if(event.keyCode === 72) {
+    if (event.keyCode === 72) {
         section01.classList.add('up');
     } else if (event.keyCode === 84) {
         section01.classList.remove('up');
@@ -75,7 +76,7 @@ function toggle(event) {
 //}
 //
 function togglee(event) {
-    if(event.keyCode === 82) {
+    if (event.keyCode === 82) {
         section02.classList.add('down');
     } else if (event.keyCode === 83) {
         section02.classList.remove('down');
@@ -89,29 +90,29 @@ console.log();
 
 
 //Hieronder staan knoppen waardoor de body kleur zal veranderen
-// APA: Just a moment... (2020, 10 april). Geraadpleegd op 17 mei 2020, van https://codepen.io/chionae/pen/xzOWvM
+// Just a moment... (2020, 10 april). Geraadpleegd op 17 mei 2020, van https://codepen.io/chionae/pen/xzOWvM
 
 function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
 
 function greenFunctie() {
-   var element = document.body;
-   element.classList.toggle("green-verloop");
+    var element = document.body;
+    element.classList.toggle("green-verloop");
 }
 
 function blauwFunctie() {
-   var element = document.body;
-   element.classList.toggle("blue-verloop");
+    var element = document.body;
+    element.classList.toggle("blue-verloop");
 }
 
 function roodFunctie() {
-   var element = document.body;
-   element.classList.toggle("verloop");
+    var element = document.body;
+    element.classList.toggle("verloop");
 }
 
 function alleKleur() {
-   var element = document.body;
-   element.classList.toggle("allekleur");
+    var element = document.body;
+    element.classList.toggle("allekleur");
 }
